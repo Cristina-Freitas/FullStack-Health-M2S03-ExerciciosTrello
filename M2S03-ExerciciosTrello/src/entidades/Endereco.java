f5244cca1c1afeebaaeda10256e321d04918be68
CrisTinaFLN <crisjf.tina@hotmail.com>
2024-07-16 14:06:22 -0300

    Ex.2: Classe Endereço

---
+++ M2S03-ExerciciosTrello/src/entidades/Endereco.java
@@ -0,0 +1,65 @@
+package entidades;
+
+//Ex 3: Classe Endereço
+public class Endereco {
+    private String logradouro;
+    private String estado;
+    private String cidade;
+    private Integer numero;
+    private Integer cep;
+
+    //Construtor vazio
+    public Endereco() {
+    }
+
+    //Construtor completo
+    public Endereco(String logradouro, String estado, String cidade, Integer numero, Integer cep) {
+        this.logradouro = logradouro;
+        this.estado = estado;
+        this.cidade = cidade;
+        this.numero = numero;
+        this.cep = cep;
+    }
+
+    //getters e setters
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public Integer getCep() {
+        return cep;
+    }
+
+    public void setCep(Integer cep) {
+        this.cep = cep;
+    }
+}
